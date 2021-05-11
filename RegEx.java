9733625c7f2a38b2c05716cfc097ed0cc80d25b4
pvijaykrishna549 <you@example.com>
2021-05-11 20:43:54 +0530

    UC5toUC8: Password Validation with atleast 1 Numeric,exactly 1 special character, 1 Upper Case, Minimum 8 Characters

--- RegEx.java
+++ RegEx.java
@@ -8,6 +8,7 @@ public static void main(String args[]) {
 	System.out.println(Pattern.matches("^[A-Z][a-z]{3,}\\s[A-Z][a-z]{3,}+$", "Vijay Krishna"));
 	System.out.println(Pattern.matches("^[a-z]{3}[.][xyz]*[@][a-z]{2}[.][co]{2}[.][in]*$", "abc.xyz@bl.co.in"));
 	System.out.println(Pattern.matches("^[1-9]{2}\\s[1-9][0-9]{9}$", "91 9915473501"));
+	System.out.println(Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#]*)[a-zA-Z0-9@!#*]{8,}", "Rohit@12"));
 	
 }
 }
