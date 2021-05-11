b5f6489d1a63f3031fe1bd8cb5fea77b7b5d8624
pvijaykrishna549 <you@example.com>
2021-05-11 20:16:00 +0530

    Second Commit: Validate User's Last Name Starting With Cap & atleast 3 Characters

--- RegEx.java
+++ RegEx.java
@@ -5,6 +5,7 @@ public static void main(String args[]) {
 
 
 	System.out.println(Pattern.matches("^[A-z][a-z]{3,}$", "Vijay"));
+	System.out.println(Pattern.matches("^[A-Z][a-z]{3,}\\s[A-Z][a-z]{3,}+$", "Vijay Krishna"));
 	
 }
 }
