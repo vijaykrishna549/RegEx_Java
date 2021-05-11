b1e57c86d72e2bb6948319e9b7964bafbb8f9073
pvijaykrishna549 <you@example.com>
2021-05-11 20:47:52 +0530

    UC-9: Email Validation For All Provided Emails

--- RegEx.java
+++ RegEx.java
@@ -9,6 +9,15 @@ public static void main(String args[]) {
 	System.out.println(Pattern.matches("^[a-z]{3}[.][xyz]*[@][a-z]{2}[.][co]{2}[.][in]*$", "abc.xyz@bl.co.in"));
 	System.out.println(Pattern.matches("^[1-9]{2}\\s[1-9][0-9]{9}$", "91 9915473501"));
 	System.out.println(Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#]*)[a-zA-Z0-9@!#*]{8,}", "Rohit@12"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc@yahoo.com"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc-100@yahoo.com,"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc.100@yahoo.com"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc111@abc,com"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc-100@abc.com.au"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc-100@abc.net"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc@1.com"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc@gmail.com.com"));
+	System.out.println(Pattern.matches("^[a-z0-9]{3,6}[.@+-][0-9a-z]{1,}[.,@][a-z]{0,8}[.]*[,.]*[a-z]{2,3}[.,]*[a-z]*[a-z]*$", "abc+100@gmail.com"));	
 	
 }
 }
