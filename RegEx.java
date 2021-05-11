cc71a8e585971e2c5bce58ff192f371ace96fe06
pvijaykrishna549 <you@example.com>
2021-05-11 20:13:01 +0530

    First Cpommit: Validate First Name Starting with Cap & Atleast 3 Characters

---
+++ RegEx.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regex;
+import java.util.regex.*;
+public class RegEx {
+public static void main(String args[]) {
+
+
+	System.out.println(Pattern.matches("^[A-z][a-z]{3,}$", "Vijay"));
+	
+}
+}
