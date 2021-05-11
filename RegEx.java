1cd75176f7d13f2b51644221557dea46d6e38e7a
pvijaykrishna549 <you@example.com>
2021-05-11 20:23:21 +0530

    Third Commit: Email Validation

--- RegEx.java
+++ RegEx.java
@@ -6,6 +6,7 @@ public static void main(String args[]) {
 
 	System.out.println(Pattern.matches("^[A-z][a-z]{3,}$", "Vijay"));
 	System.out.println(Pattern.matches("^[A-Z][a-z]{3,}\\s[A-Z][a-z]{3,}+$", "Vijay Krishna"));
+	System.out.println(Pattern.matches("^[a-z]{3}[.][xyz]*[@][a-z]{2}[.][co]{2}[.][in]*$", "abc.xyz@bl.co.in"));
 	
 }
 }
